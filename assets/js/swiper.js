4747bb4d777e953819944848d3d832045bab9e57
phivu99 <lephivu99@gmail.com>
2022-04-19 17:31:26 +0700

    up

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -49,6 +49,35 @@ var swiper =  new Swiper(".carousel", {
     },
 });
 
+var swiper =  new Swiper(".trendy", {
+    // autoplay: {
+    //     delay: 5000,
+    //     disableOnInteraction: false,
+    //     },
+    loop:false,
+    slidesPerView: 1,
+    slidesPerColumn: 2,
+    slidesPerColumnFill: 'row',
+    spaceBetween: 30,
+    navigation: {
+        clickable: true,
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        576: {
+            slidesPerView: 2,
+            slidesPerColumn: 2,
+            spaceBetween: 30,
+        },
+        992: {
+            slidesPerView: 3,
+            slidesPerColumn: 2,
+            spaceBetween: 30,
+        },
+    },
+});
+
 var swiper =  new Swiper(".carousel-overflow", {
     // autoplay: {
     //     delay: 5000,
