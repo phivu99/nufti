6d75a7178516d1fcda03b982c743e25b982ac91c
unknown <lephivu99@gmail.com>
2022-05-16 17:05:57 +0700

    up

--- assets/js/main.js
+++ assets/js/main.js
@@ -130,10 +130,12 @@
         $(".light").removeClass("is_active")
         $(".dark").addClass("is_active")
 
-        $(".tf-text").removeClass("style")
-        $(".tf-text").addClass("s1")
+        // $(".tf-text").removeClass("style")
+        // $(".tf-text").addClass("s1")
+        // $('#avatar-01').attr('src','assets/images/avatar/avt-01.png');
         document.getElementById("logo_header").src = "assets/images/logo/logo_dark.png";
         document.getElementById("logo_footer").src = "assets/images/logo/logo_dark.png";
+        // document.getElementById("avatar-01").src = "assets/images/avatar/avt-01.png";
     });
 
     $(".light").on('click', function (e) {
@@ -144,8 +146,17 @@
 
         // $(".tf-text").addClass("style")
         // $(".tf-text").removeClass("s1")
+        // $('#avatar-01').attr('src','assets/images/avatar/avt-1.jpg');
         document.getElementById("logo_header").src = "assets/images/logo/logo.png";
         document.getElementById("logo_footer").src = "assets/images/logo/logo2.png";
+        // document.getElementById("avatar-01").src = "assets/images/avatar/avt-1.jpg";
+        // document.getElementById("avatar-01").src = "assets/images/avatar/avt-02.png";
+    });
+
+    $('.light').on({
+        'click': function(){
+            $('#avatar-01').attr('assets/images/avatar/','avt.jpg');
+        }
     });
 
     // Header Connect
